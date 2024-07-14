e13e3c19d2f3c56efff77a4e743152a9b477fe08
adzaky <muhadlim17@gmail.com>
2024-07-14 14:16:44 +0800

    feat(comp): create navbar

--- src/features/Dashboard/index.tsx
+++ src/features/Dashboard/index.tsx
@@ -1,51 +1,12 @@
 'use client';
 
-import { Button } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { useAuthLogout } from '../Auth/hooks';
-import { useGetUsers } from './hooks';
-import { fetcherAuth } from '@/lib/axios';
+import NavigationBar from '@/components/Navbar';
 
 const DashboardPage = () => {
-  const { push } = useRouter();
-
-  //   const { data: userData } = useGetUsers({ enabled: fetchUsers });
-
-  const { mutate: logout } = useAuthLogout({
-    onSuccess: () => {
-      console.log('logout');
-    },
-    onError: (error: any) => {
-      console.log(error);
-    }
-  });
-
-  const handelLogout = () => {
-    logout();
-    push('/sign-in');
-  };
-
-  const handleGetUsers = async () => {
-    try {
-      const userData = await fetcherAuth.get('/users/profile');
-
-      console.log(userData.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
-    <>
-      <div>DashboardPage</div>
-      <Button color="danger" onClick={handelLogout}>
-        Logout
-      </Button>
-      <Button color="danger" onClick={handleGetUsers}>
-        set token
-      </Button>
-    </>
+    <div className="min-h-screen bg-gray-700">
+      <NavigationBar />
+    </div>
   );
 };
 
