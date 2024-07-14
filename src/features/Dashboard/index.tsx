aa5ddf7f3a882d79ed4777374356b451bb4e3c4d
adzaky <muhadlim17@gmail.com>
2024-07-14 20:27:20 +0800

    feat(features/Dashboard): remove unneeded testing hooks

--- src/features/Dashboard/index.tsx
+++ src/features/Dashboard/index.tsx
@@ -1,52 +1,5 @@
-'use client';
-
-import { Button } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { useAuthLogout } from '../Auth/hooks';
-import { useGetUsers } from './hooks';
-import { fetcherAuth } from '@/lib/axios';
-
 const DashboardPage = () => {
-  const { push } = useRouter();
-
-  //   const { data: userData } = useGetUsers({ enabled: fetchUsers });
-
-  const { mutate: logout } = useAuthLogout({
-    onSuccess: () => {
-      console.log('logout');
-    },
-    onError: (error: any) => {
-      console.log(error);
-    }
-  });
-
-  const handelLogout = () => {
-    logout();
-    push('/sign-in');
-  };
-
-  const handleGetUsers = async () => {
-    try {
-      const userData = await fetcherAuth.get('/users/profile');
-
-      console.log(userData.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <>
-      <div>DashboardPage</div>
-      <Button color="danger" onClick={handelLogout}>
-        Logout
-      </Button>
-      <Button color="danger" onClick={handleGetUsers}>
-        set token
-      </Button>
-    </>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default DashboardPage;
