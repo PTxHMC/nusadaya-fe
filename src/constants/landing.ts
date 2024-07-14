237a9684db36c5ac6bd5827bdb351b651ecffa0d
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-07-14 21:13:29 +0800

    feat: add navbar at landing page

---
+++ src/constants/landing.ts
@@ -0,0 +1,16 @@
+import { NavbarType } from '@/types';
+
+export const NavbarData: NavbarType[] = [
+  {
+    id: 1,
+    title: 'Belajar'
+  },
+  {
+    id: 2,
+    title: 'Tentang Kami'
+  },
+  {
+    id: 3,
+    title: 'Hubungi Kami'
+  }
+];
