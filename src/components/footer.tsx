e5b9989d9cf46c2b0a6fc90c79dcf7c75870b5ec
adzaky <muhadlim17@gmail.com>
2024-07-17 23:22:04 +0800

    feat(comp/Footer): create footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="relative bottom-0 flex flex-col items-center justify-between gap-2 text-nowrap bg-primary-1 px-12 py-4 text-xs text-white-1 sm:flex-row">
+      <p>
+        Copyright &copy; 2024{' '}
+        <span className="text-primary-text">Nusadaya</span>
+      </p>
+      <p>All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
